e33c37587cb814b02c572b05d53ddd0d244b3f66
Alan Foster <alan-af@outlook.com>
2022-08-30 16:21:19 -0300

    2

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// rendeziar a mensagem de "Carregando"
+class Loading extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Carregando...</h2>
+      </div>
+    );
+  }
+}
+
+export default Loading;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
-export default class Login extends Component {
+class Login extends React.Component {
+  state = {
+    name: '',
+    loading: false,
+    logged: false,
+  };
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleonClick = async () => {
+    const { name } = this.state;
+    this.setState({ loading: true });
+    await createUser({ name })
+      .then(() => {
+        this.setState({ loading: false, logged: true });
+      });
+  };
+
   render() {
+    const minCaracters = 3;
+    const { name, loading, logged } = this.state;
+    if (loading) return <Loading />;
+    if (logged) return <Redirect to="/search" />;
     return (
-      <div data-testid="page-login">
-        <input data-testid="login-name-input" type="text" placeholder="Loguin" />
+      <div
+        id="login"
+        data-testid="page-login"
+      >
+        <form>
+          <input
+            id="name"
+            type="text"
+            data-testid="login-name-input"
+            onChange={ this.handleChange }
+            placeholder="Loguin"
+          />
+          <button
+            data-testid="login-submit-button"
+            type="button"
+            onClick={ this.handleonClick }
+            disabled={ name.length < minCaracters }
+          >
+            Entrar
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+export default Login;
