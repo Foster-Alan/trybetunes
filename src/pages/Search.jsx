e82b8973aa798b40c0aed61b9aa94c03e948dbcc
Alan Foster <alan-af@outlook.com>
2022-08-30 18:51:43 -0300

    5 e 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,13 +1,114 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
-export default class Search extends Component {
+class Search extends React.Component {
+  state = {
+    artist: '',
+    albums: [],
+    serch: '',
+    ButtonDisabled: true,
+    loading: false,
+  };
+
+  // Função procurar nome
+
+  handlechange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { artist } = this.state;
+      if (artist.length > 1) {
+        this.setState({ ButtonDisabled: false });
+      }
+    });
+  };
+
+  // Função botão
+
+  handleclick = () => {
+    const { artist } = this.state;
+    this.setState(
+      { loading: true, serch: artist },
+      async () => {
+        const albums = await searchAlbumsAPI(artist);
+        this.setState({
+          albums,
+          artist: '',
+          loading: false,
+        });
+      },
+
+    );
+  };
+
   render() {
+    const {
+      artist,
+      albums,
+      serch,
+      ButtonDisabled,
+      loading,
+    } = this.state;
     return (
       <div data-testid="page-search">
-        Search
         <Header />
+        {loading ? <Loading /> : (
+          <div>
+            <input
+              type="text"
+              name="artist"
+              data-testid="search-artist-input"
+              placeholder="Banda / Artista"
+              value={ artist }
+              onChange={ this.handlechange }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ ButtonDisabled }
+              onClick={ this.handleclick }
+            >
+              Pesquisar
+            </button>
+          </div>
+        )}
+        <div>
+          {!albums.length ? 'Nenhum álbum foi encontrado' : (
+            <div>
+              <p>{`Resultado de álbuns de: ${serch}`}</p>
+              {albums.map(({
+                artistName,
+                collectionId,
+                collectionName,
+                collectionPrice,
+                artworkUrl100,
+                releaseDate,
+                trackCount,
+              }) => (
+                <div key={ collectionId }>
+                  <img src={ artworkUrl100 } alt={ artistName } />
+                  <h4>{ artistName }</h4>
+                  <p>{ collectionPrice }</p>
+                  <p>{ releaseDate }</p>
+                  <p>{ trackCount }</p>
+                  <Link
+                    to={ `/album/${collectionId}` }
+                    data-testid={ `link-to-album-${collectionId}` }
+                  >
+                    {collectionName}
+                  </Link>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     );
   }
 }
+
+export default Search;
