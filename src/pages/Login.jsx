72d68cb2ab6b785198295289de4f0c6f6b3c2290
Alan Foster <alan-af@outlook.com>
2022-08-30 15:09:19 -0300

    1 novamente

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class Login extends Component {
   render() {
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        <input data-testid="login-name-input" type="text" placeholder="Loguin" />
+      </div>
     );
   }
 }
